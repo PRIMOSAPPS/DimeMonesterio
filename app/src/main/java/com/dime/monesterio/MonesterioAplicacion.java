492057e3a3060bbc0c56651706882a2aad819001
h <h@h.com>
2016-01-10 11:09:39 +0100

    Primer commit de esta aplicación

---
+++ app/src/main/java/com/dime/monesterio/MonesterioAplicacion.java
@@ -0,0 +1,21 @@
+package com.dime.monesterio;
+
+import android.util.Log;
+
+import com.modulos.libreria.dimepoblacioneslibreria.aplicacion.DimeApplication;
+import com.modulos.libreria.dimepoblacioneslibreria.singleton.SingletonDimePoblaciones;
+
+/**
+ * Created by h on 29/10/15.
+ */
+public class MonesterioAplicacion extends DimeApplication {
+
+    private final static String TAG = "[MonesterioAplicacion]";
+
+    public MonesterioAplicacion() {
+        super();
+        Log.d(TAG, "onCreate()");
+        SingletonDimePoblaciones.getInstance().setIdIconoNotificacion(R.drawable.ic_action_notificacion);
+        SingletonDimePoblaciones.getInstance().setIdLogoPoblacion(R.drawable.escudo_monesterio_logo);
+    }
+}
