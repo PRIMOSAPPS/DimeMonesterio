fd9b938649d1bf120412bcd0508394a6fbdacb85
h <h@h.com>
2016-07-06 19:16:55 +0200

    Cambios para volver a recibir las notificaciones desde parse

--- app/src/main/java/com/dime/monesterio/receiver/MonesterioComprobadorActualizacionesBroadcastReceiver.java
+++ app/src/main/java/com/dime/monesterio/receiver/MonesterioComprobadorActualizacionesBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.dime.monesterio.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.dime.monesterio.service.MonesterioServicioComprobadorActualizaciones;
 
@@ -10,11 +11,13 @@ import com.dime.monesterio.service.MonesterioServicioComprobadorActualizaciones;
  * Created by h on 6/03/16.
  */
 public class MonesterioComprobadorActualizacionesBroadcastReceiver extends BroadcastReceiver {
+    private final static String TAG = "[MonesComprActBR]";
 
     public MonesterioComprobadorActualizacionesBroadcastReceiver() {}
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "MonesterioComprobadorActualizacionesBroadcastReceiver.onReceive()");
         Intent startServiceIntent = new Intent(context, MonesterioServicioComprobadorActualizaciones.class);
         context.startService(startServiceIntent);
     }
